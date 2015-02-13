ddf8ba847f937771cf4f53a1d78935102d331782
Emanuele Corradini <korra88@gmail.com>
2015-02-13 17:14:32 +0100

    Removed 'auto-embedding' from track ID, localized text in italian, removed unused code, added simple tuttorial in the dialog.

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,29 +1,16 @@
-tinyMCEPopup.requireLangPack();
-
 var SoundcloudDialog = {
-	init : function() {
-        // Should load up the YouTube content and fill the field, but you
-        // can't select the video at this point.
-	},
-
-	insert : function() {
+    init: function () {
+    },
+    insert: function () {
         var sciframe = document.forms[0].sciframe.value;
-        var scid = document.forms[0].scid.value;
         var scdata = '';
 
-        if (scid !== '') {
-            scdata = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F' + scid + '"></iframe>';
-        } else {
-            scdata = sciframe;
-        }
+        scdata = sciframe;
         var html = '<div class="soundcloud soundcloud-track">' + scdata + '</div>';
-		tinyMCEPopup.editor.execCommand('mceInsertContent', false, html);
-		tinyMCEPopup.close();
-	},
-
-    close : function () {
-        tinyMCEPopup.close();
+        top.tinymce.activeEditor.insertContent(html);
+        this.close();
+    },
+    close: function () {
+        top.tinymce.activeEditor.windowManager.close();
     }
 };
-
-tinyMCEPopup.onInit.add(SoundcloudDialog.init, SoundcloudDialog);
