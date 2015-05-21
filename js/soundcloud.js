d473b8e91b415c4ac04eaaba848d5f4ee27b415e
Davide Cavallini <dcavallinister@gmail.com>
2015-05-21 13:13:52 +0200

    Full refactoring, added localisation in de,en,it. Now you can paste only the share link and it gives a preview too.

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,16 +1,73 @@
-var SoundcloudDialog = {
-    init: function () {
-    },
-    insert: function () {
-        var sciframe = document.forms[0].sciframe.value;
-        var scdata = '';
+//Langs. To add or adjust the lang files are in the lang dir
+var data = {
+    "scegliTraccia": parent.tinymce.util.I18n.translate('Scegli traccia'),
+    "menuCondivisione": parent.tinymce.util.I18n.translate('menu condivisione'),
+    "cliccaEmbed": parent.tinymce.util.I18n.translate('clicca Embed'),
+    "incollaCodice": parent.tinymce.util.I18n.translate('Incolla codice'),
+    "incollaQui": parent.tinymce.util.I18n.translate('Incolla qui'),
+    "inserisci": parent.tinymce.util.I18n.translate('inserisci'),
+    "chiudi": parent.tinymce.util.I18n.translate('chiudi'),
+    "share": parent.tinymce.util.I18n.translate('share copia'),
+    "anteprima": parent.tinymce.util.I18n.translate('anteprima'),
+};  
+function soundcloudDialog() {
+    $(function () {        
+        $('#insert').click(function () {
+            I_Insert();
+            I_Close();
+            return false;
+        });
+        $('#cancel').click(function () {
+            I_Close();
+            return false;
+        });
+        $('#inpURL').keyup(function () {
+           selectAudio(true);
+        });        
+    });
+}
 
-        scdata = sciframe;
-        var html = '<div class="soundcloud soundcloud-track">' + scdata + '</div>';
-        top.tinymce.activeEditor.insertContent(html);
-        this.close();
-    },
-    close: function () {
-        top.tinymce.activeEditor.windowManager.close();
+function selectAudio(mini) {
+    if(mini !== undefined) {
+        $('#preview').html(get_video_iframe(true));
+    }else {
+        $('#preview').html(get_video_iframe());
     }
-};
+}
+
+function I_InsertHTML(sHTML) {
+    if (getVideoId($('#inpURL').val()) == '') {
+        return false;
+    }
+    parent.tinymce.activeEditor.insertContent(sHTML);
+}
+
+function I_Close() {
+    parent.tinymce.activeEditor.windowManager.close();
+}
+
+function get_video_iframe(mini) {
+    var width = 670;
+    var sEmbedUrl = 'https://w.soundcloud.com/player/?url=' + getVideoId($('#inpURL').val()) + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true';
+    if(mini !== undefined) {
+        width = 600;
+    }
+    var sHTML = '<iframe class="soundcloud" width="' + width + '" height="200" src="' + sEmbedUrl + '" frameborder="0" allowfullscreen></iframe>';
+    return sHTML;
+}
+function I_Insert() {    
+    I_InsertHTML(get_video_iframe());
+}
+
+function getVideoId(url) {
+    return url.replace(/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/, '$5');
+}
+
+//Use jQuery's get method to retrieve the contents of our template file, then render the template.
+$.get('template/form.html', function (template) {
+    console.log(template);
+    filled = Mustache.render(template, data);
+    $('#template-container').append(filled);
+    
+    soundcloudDialog();
+});
---
+++ langs/de_DE.js
@@ -0,0 +1,11 @@
+tinymce.addI18n('de_DE',{
+    'Scegli traccia' : 'Scegli la tua traccia su',
+    'menu condivisione' : 'e apri il menù di condivisione',
+    'clicca Embed' : 'Clicca "Embed" in alto, copia il codice',
+    'Incolla codice': 'Incolla qui il codice',
+    'Incolla qui': 'Incolla il codice qui!',
+    'inserisci': 'Inserisci',
+    'chiudi': 'Chiudi',    
+    'share copia' : 'Copia il link di condivisione',    
+    'anteprima': 'Anteprima'
+});
